f601a144f1ceccbf67a53d46348dcb99c8a30e08
Wenish <Wenish@users.noreply.github.com>
2025-04-23 16:26:11 +0200

    fix(ui): correct div nesting and improve newsletter text layout
    Fix incorrect closing div tag to ensure proper HTML structure on the
    home page. Adjust the newsletter paragraph to wrap text for better
    readability and consistent styling. These changes improve page layout
    and user experience.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -6,11 +6,12 @@
       <div class="max-w-6xl mx-auto py-4">
         <a href="/world-map" class="btn btn-soft">Zur Weltkarte</a>
       </div>
-  </div>
+    </div>
     <div class="bg-base-300 py-16 mt-16">
       <div class="max-w-6xl mx-auto py-4">
         <h2 class="text-6xl pb-4">Newsletter</h2>
-        <p class="w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p class="w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.</p>
         <fieldset class="fieldset  bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend class="fieldset-legend">Was ist deine E-Mail?</legend>
           <input type="text" class="input" placeholder="Hier eingeben" />
