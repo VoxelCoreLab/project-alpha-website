5c4263d709d5a24b1a3d8241e0aaab0dbe9739e6
Wenish <Wenish@users.noreply.github.com>
2025-04-23 21:59:32 +0200

    feat(home): enhance newsletter section and add release info (#14)
    Increase padding in the newsletter section for better spacing and update
    the description to clearly inform users about the newsletter content. 
    Improve input placeholder text for clarity. Add a new section detailing 
    the expected beta phase and official game release dates to keep users 
    informed about the project timeline.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -7,18 +7,25 @@
         <a href="/world-map" class="btn btn-soft">Zur Weltkarte</a>
       </div>
     </div>
-    <div class="bg-base-300 py-16 mt-16">
+    <div class="bg-base-300 py-32 mt-16">
       <div class="max-w-6xl mx-auto py-4">
         <h2 class="text-6xl pb-4">Newsletter</h2>
-        <p class="w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua.</p>
-        <fieldset class="fieldset  bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <p class="w-lg">Erhalte News zum Spiel direkt in dein Postfach. Wir informieren dich über Updates, Patches, den
+          Start der Beta und die Veröffentlichung des Spieles.</p>
+        <fieldset class="fieldset  bg-base-200 border-base-300 rounded-box w-xs border p-4 mt-4">
           <legend class="fieldset-legend">Was ist deine E-Mail?</legend>
-          <input type="text" class="input" placeholder="Hier eingeben" />
+          <input type="text" class="input" placeholder="Hier deine E-Mail eingeben" />
           <!-- <p class="label">Optional</p> -->
 
           <button class="btn btn-neutral mt-4">Beim Newsletter anmelden</button>
         </fieldset>
+      </div>
+    </div>
+    <div class="bg-base-200 py-32">
+      <div class="max-w-6xl mx-auto py-4">
+        <h2 class="text-6xl pb-4">Spielveröffentlichung</h2>
+        <p class="w-lg">Die Beta-Phase ist voraussichtlich Ende 2025</p>
+        <p class="w-lg">Die offizielle Veröffentlichung ist für das Jahr 2026 geplant.</p>
       </div>
     </div>
   </LayoutBasic>
