42ff6c28a450a3062b2a5d1e3768242acbdf3241
Wenish <Wenish@users.noreply.github.com>
2025-04-23 02:20:03 +0200

    Update PageHome.vue

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <LayoutBasic>
-    <div>Page Home</div>
+    <h1 class="text-8xl font-bold max-w-6xl mx-auto py-8">Game Title</h1>
   </LayoutBasic>
 </template>
 <script setup lang="ts">
