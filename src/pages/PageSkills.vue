bc8b22ddca2ff01966ca08d35a5b363a2b18bdcb
Wenish <Wenish@users.noreply.github.com>
2025-05-11 18:45:03 +0200

    feat: rename gameplay route to skills and add FAQ content (#57)
    Rename the '/gameplay' route to '/skills' and update the header link 
    to reflect this change. Add a new FAQ page with a list of common 
    questions and answers to improve user support and provide essential 
    game information.

---
+++ src/pages/PageSkills.vue
@@ -0,0 +1,15 @@
+<template>
+    <LayoutBasic :breadcrumbs="breadcrumbs">
+        Page Fähigkeiten
+    </LayoutBasic>
+</template>
+<script setup lang="ts">
+import LayoutBasic from '../layouts/LayoutBasic.vue'
+
+const breadcrumbs = [
+    {
+        text: 'Fähigkeiten',
+        href: '/skills'
+    }
+]
+</script>
