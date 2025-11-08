176cb232baa89337d0b891fa71460c2bc4636173
Wenish <Wenish@users.noreply.github.com>
2025-11-08 02:04:43 +0100

    Update TheHeader.vue

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,7 +3,7 @@
         <div class="navbar bg-base-100 max-w-7xl mx-auto">
             <div class="navbar-start w-auto">
                 <div class="flex-none md:hidden">
-                    <label for="my-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost" role="button" tabindex="0"
+                    <label for="my-drawer" aria-label="open sidebar" class="btn btn-square rounded btn-ghost" role="button" tabindex="0"
                         @keydown.enter.prevent="openDrawer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             class="inline-block h-6 w-6 stroke-current">
@@ -12,7 +12,7 @@
                         </svg>
                     </label>
                 </div>
-                <a class="btn btn-ghost text-xl " href="/"><img class="h-8 w-8" alt="" src="../assets/logo.png"></a>
+                <a class="btn btn-ghost text-xl rounded" href="/"><img class="h-8 w-8" alt="" src="../assets/logo.png"></a>
             </div>
             <div class="navbar-center hidden md:flex">
                 <ul class="menu menu-horizontal px-1">
