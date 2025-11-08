6c4056aca919a00ccaf5e569e41284b9224674a9
Wenish <Wenish@users.noreply.github.com>
2025-11-08 02:55:40 +0100

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,30 @@
 <template>
-    <footer class="footer footer-horizontal footer-center bg-base-100 text-base-content rounded p-10 pt-16 md:pt-32">
+    <footer class="footer footer-horizontal footer-center bg-base-100 text-base-content rounded p-10 pt-16 md:pb-16">
+        <div class="py-16">
+            <div class="uppercase font-bold text-secondary/50">Socials</div>
+            <div class="">
+                <div class="grid grid-flow-col gap-4">
+
+                    <a href="https://www.youtube.com/@ShadowInfectionGame" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            class="fill-current">
+                            <path
+                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z">
+                            </path>
+                        </svg>
+                    </a>
+                    <a href="https://www.tiktok.com/@shadowinfection" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            class="fill-current">
+                            <path
+                                d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1.04-.1z">
+                            </path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+                
+        </div>
         <nav class="flex flex-wrap justify-center gap-4">
             <a v-for="link in links" :key="link.name" class="link link-hover" :href="link.path">{{ link.name }}</a>
         </nav>
