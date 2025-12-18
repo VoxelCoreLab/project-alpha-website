6ffae0ed84d99c7304cba4715fe17154487dd2a3
Wenish <Wenish@users.noreply.github.com>
2025-12-18 19:03:48 +0100

    Update PageCheckout.vue

--- src/pages/PageCheckout.vue
+++ src/pages/PageCheckout.vue
@@ -209,8 +209,7 @@
                                                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
                                                 </path>
                                             </svg>
-                                            <span class="text-xs">Final tax calculation will be shown on the payment
-                                                overview</span>
+                                            <span class="text-xs">All prices include applicable taxes.</span>
                                         </div>
                                     </div>
                                     <!--
